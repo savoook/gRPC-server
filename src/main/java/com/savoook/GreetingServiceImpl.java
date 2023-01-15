298ea0ccba4dee6e30cac198b12ed9475d0af8c2
Pavel Savin <pavsav1987@gmail.com>
2023-01-15 20:45:52 +0400

    15.01.2023 - init

---
+++ src/main/java/com/savoook/GreetingServiceImpl.java
@@ -0,0 +1,18 @@
+package com.savoook;
+
+import io.grpc.stub.StreamObserver;
+
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+    @Override
+    public void greeting(GreetingServiceOuterClass.HelloRequest request, StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
+        System.out.println(request);
+
+        GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse
+                .newBuilder()
+                .setGreeting("Hello from server, " + request.getName())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
